c971cefdfd82f543b92e4287aa7267ee3595d300
Bridget-Kathure <bridgetkathuretitus@gmail.com>
2024-02-21 20:39:46 +0300

    practice kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    var name: String = "Bridget Kathure"
+    println(name)
+    var age: Byte= 22
+    println(age)
+    var phoneNumber: String = "110125745"
+    println(phoneNumber)
+    var weight: Float = 66.5F
+    println(weight)
+    var kenyanCitizen: Boolean = true
+    println(kenyanCitizen)
+}
